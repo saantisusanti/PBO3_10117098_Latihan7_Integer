6454687169389fbaf885456b9e0a2f9bb4e1e932
Santi Susanti <43363256+saantisusanti@users.noreply.github.com>
2018-09-23 00:15:21 +0700

    Add files via upload

---
+++ PBO3_10117098_Latihan7_Integer/src/pbo3_10117098_latihan7_integer/PBO3_10117098_Latihan7_Integer.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117098_latihan7_integer;
+
+/**
+ *
+ * @author 
+ * NAMA  : Santi Susanti
+ * KELAS : PBO3
+ * NIM   : 10117098
+ */
+public class PBO3_10117098_Latihan7_Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte\t : " + b);
+        System.out.println("short\t : " + s);
+        System.out.println("int\t : " + i);
+        System.out.println("long\t : " + l);
+    }
+    
+}
